6538dfa7f6bfbf5cae31dc9835c64c746594a92e
ashrafabueraq <ashrafabueraq97@gmail.com>
2022-06-22 01:49:56 +0300

    task2

--- ClassComponents_21-JUNE-2022 (Task2)/src/classcomponent/cards.js
+++ ClassComponents_21-JUNE-2022 (Task2)/src/classcomponent/cards.js
@@ -39,6 +39,17 @@ class Card extends React.Component {
                     </div>
 
                 </div>
+
+                <div class="card">
+                    <img src={Un} className="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <button type="button" class="btn btn-primary">Click here</button>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+
+                </div>
             </div>
 
         );
